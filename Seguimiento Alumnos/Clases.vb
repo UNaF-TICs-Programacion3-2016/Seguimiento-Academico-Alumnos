ed40d13619b3bfa2830e35ca37f00518d5ec81f8
carlos9307 <acosta.c9307@gmail.com>
2016-11-10 22:46:35 -0300

    Clase Alumno
    Se crea la clase Alumno

--- Seguimiento Alumnos/Clases.vb
+++ Seguimiento Alumnos/Clases.vb
@@ -1,4 +1,13 @@
 ﻿Public Class Alumno
+    Private _Nombre As String
+    Private _Apellido As String
+    Private _Documento As Integer
+    Private _FechaNac As Date
+    Private _Telefono As Integer
+    Private _Localidad As Integer
+    Private _Calle As String
+    Private _Altura As Integer
+    Private _EstadoCivil As Integer
 
 
 
